c0f938259b891914de33253259736b2e4afee34e
DarkDragon <tylerliston16@gmail.com>
2024-06-22 23:58:25 -0400

    Grenade
    Grenade does damage and destroy but the particles need to destroy

--- FPS-PP1/Assets/Scripts/GrenadeThrower.cs
+++ FPS-PP1/Assets/Scripts/GrenadeThrower.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Data;
 using UnityEngine;
 
 public class Forcethrow : MonoBehaviour
@@ -7,7 +8,7 @@ public class Forcethrow : MonoBehaviour
     //throw force is for travel
     [SerializeField] int throwForce;
     //this if for hieght
-    [SerializeField] int ThrowHieght;
+    [SerializeField] float ThrowHieght;
     //gravity
     [SerializeField] int throwGravity;
 
@@ -15,9 +16,11 @@ public class Forcethrow : MonoBehaviour
     [SerializeField] int playerHold;
     [SerializeField] int MaxGrenade;
 
+    //starting postion is needded 
+    
     Vector3 GrenadeVelocity;
     //[SerializeField] int ThrowSpeed;
-
+    [SerializeField] int delayTime;
     [SerializeField] GameObject grenadeP;
 
     // Update is called once per frame
@@ -27,24 +30,37 @@ public class Forcethrow : MonoBehaviour
         if (Input.GetButtonDown("Grenade") && playerHold < MaxGrenade)
         {
             ThrowGrenade();
-            GrenadeVelocity.y = throwForce;
-            GrenadeVelocity.y -= throwGravity * Time.deltaTime;
+            GrenadeVelocity = transform.forward * throwForce;
+            GrenadeVelocity.y = transform.forward.y * ThrowHieght;
+            GrenadeVelocity.y = throwGravity;
             playerHold--;
           // GrenadeVelocity.y = ThrowSpeed;
             GameManager.instance.GrenadeAmmoText.text = playerHold.ToString("F0");
         }
+        GrenadeVelocity.y -= throwForce * Time.deltaTime;
+        
+        
+    }
+    //collistion for the grenade is not here
+
+    void ReverseGround()
+    {
 
     }
-
     void ThrowGrenade()
     {
+        //if(grenadeP )
         GrenadeVelocity = Vector3.zero;
        
         GameObject grenade = Instantiate(grenadeP,transform.position, transform.rotation);
         Rigidbody gb = grenade.GetComponent<Rigidbody>();
         gb.velocity = transform.forward * throwForce;
-        /*GrenadeVelocity.y = throwForce;
-        GrenadeVelocity.y -= throwGravity * Time.deltaTime;*/
+        //throwForce * -1;
+        GrenadeVelocity.y = throwForce;
+        GrenadeVelocity.x = throwForce;
+        //multi the throwspeed by -1
+        GrenadeVelocity.y -= throwForce * Time.deltaTime;
         gb.AddForce(transform.forward *  throwForce, ForceMode.VelocityChange);
+        Destroy(grenade,delayTime);
     }
 }
--- FPS-PP1/Assets/Scripts/PGrenade.cs
+++ FPS-PP1/Assets/Scripts/PGrenade.cs
@@ -3,55 +3,84 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PGrenade : MonoBehaviour
-{
+{//throwforce needs to be its own script for the player 
     [SerializeField] Rigidbody gb;
     [SerializeField] Renderer[] model;
 
     [SerializeField] ParticleSystem explosionEffect;
-
+    //disabling the collision
+    public bool disable = false;
     //set a delay
-    public float delay = 3f;
-    [SerializeField] float Radius; 
+   /* public int delay = 2;*/
+    //[SerializeField] int delay;
+    [SerializeField] int Radius; 
     [SerializeField] float countdown;
     bool hasExploded = false;
     [SerializeField] float force;
 
     [SerializeField] int damage;
     [SerializeField] int destroyTime;
-   
+ 
     void Start()
     {
-        countdown = delay;
+        //countdown = delay;
+        
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        countdown = delay;
+       /* countdown = delay;*/
         if (countdown <= 0f && !hasExploded)
         {
             Explode();
+            //OnTriggerEnter();
             hasExploded = true;
         }
+        countdown -= .5f;
+
     }
-    
-    void Explode()
+    //trigger for the grenade 
+    //it should travel at 3 floats
+   
+    public void OnTriggerEnter(Collider other)
     {
-        //Explostion has to Show Effects
-        Instantiate(explosionEffect,transform.position, transform.rotation);
-        //explosionEffect.Play();
-        // get nearby object and destroy them
-        Collider[] colliderToDestroy = Physics.OverlapSphere(transform.position, Radius);
-       
-        foreach (Collider nearbyObject in colliderToDestroy)
+        //the trigger needs to be disable and then turn on when used
+        //taking dmg apon entering
+        if (!disable)
         {
-            //Damage 
-            Shatter dest = nearbyObject.GetComponent<Shatter>();
-            if(dest != null)
+            IDamage dmg = other.gameObject.GetComponent<IDamage>();
+            //needs to look for and take dmg
+            if (dmg != null)
             {
-                dest.Destroy();
+                Explode();
+                dmg.TakeDamage(damage);
             }
         }
+        /*if (other.CompareTag("Player") || other.CompareTag("EnemyAI"))
+        {
+            //disable = true;
+
+          
+        }*/
+    }
+    //if the trigger works this should not be needed
+    /* public void OnCollisionEnter(Collision other)
+     {
+
+         //timer needs to end and damage anything in its radius
+         //damage 
+         IDamage dmg = other.gameObject.GetComponent<IDamage>();
+
+         if (dmg != null)
+         {
+             Explode();
+             dmg.TakeDamage(damage);
+         }
+     }*/
+    public void Knockback()
+    {
         Collider[] colliderToMove = Physics.OverlapSphere(transform.position, Radius);
         //add force to the object and move them 
         foreach(Collider nearbyObject in colliderToMove)
@@ -66,7 +95,41 @@ public class PGrenade : MonoBehaviour
         }
         // get nearby object
 
+    }
+    void Explode()
+    {
+        //Explostion has to Show Effects
+        ParticleSystem exp = Instantiate(explosionEffect,transform.position, transform.rotation);
+        //explosionEffect.Play();
+        // get nearby object and destroy them
+        Collider[] colliderToDestroy = Physics.OverlapSphere(transform.position, Radius);
 
-        Destroy(gameObject);
+        foreach (Collider nearbyObject in colliderToDestroy)
+        {
+            //Damage 
+            IDamage dest = nearbyObject.GetComponent<IDamage>();
+            if(dest != null)
+            {
+                dest.TakeDamage(damage);
+                Knockback();
+            }
+        }
+
+       /* Collider[] colliderToMove = Physics.OverlapSphere(transform.position, Radius);
+        //add force to the object and move them 
+        foreach(Collider nearbyObject in colliderToMove)
+        {
+            //Add Force 
+            Rigidbody gb = nearbyObject.GetComponent<Rigidbody>();
+            if (gb != null)
+            {
+                gb.AddExplosionForce(force, transform.position, Radius);
+            }
+
+        }*/
+        // get nearby object
+
+        
+        Destroy(exp, destroyTime);
     }
 }
